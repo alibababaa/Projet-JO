898e4bae2fd296ed8ba076340eb2166ed3ca706e
Ilyesfellah <ilyesfellah.if@gmail.com>
2024-05-08 16:20:41 +0200

    structure épreuve.c

---
+++ structure épreuve.c
@@ -0,0 +1,7 @@
+typedef struct{
+        char epreuveA;    //100m
+        char epreuveB;    //400m
+        char epreuveC;    //5000m
+        char epreuveD;    //marathon
+        char epreuveE;    //4*400m
+}Epreuve;
