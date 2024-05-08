690d4925050e1f6eca3ba4bb2118269fcefe2ae3
alibababaa <yildizaliulas@gmail.com>
2024-05-08 16:31:52 +0200

    Update jo.c

--- jo.c
+++ jo.c
@@ -2,20 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Structure pour stocker les informations d'un entraînement
-typedef struct {
-    char date[20]; // Date de l'épreuve
-    char epreuve[20]; // Type d'épreuve
-    float temps; // Temps de la performance
-    int position; // Utilisé uniquement pour le relais
-} Entrainement;
-
-// Structure pour stocker les informations d'un athlète
-typedef struct {
-    char name[50]; 
-    Entrainement *entrainements; // Détails d'un entraînement spécifique de l'athlète
-    int num_entrainement; // Nombre d'entrainements enregistrés
-} Athlete;
 
 // Structure pour stocker les differentes épreuves 
 typedef struct{
@@ -24,6 +10,28 @@ typedef struct{
         char epreuveC;    //5000m
         char epreuveD;    //marathon
         char epreuveE;    //4*400m
-}Epreuve;
+} Epreuve;
+
+// Structure pour stocker les informations d'un entraînement
+typedef struct {
+    char date[20];       // Date de l'épreuve
+    Epreuve epreuve[20]; // Type d'épreuve
+    float temps;         // Temps de la performance
+    int position;        // Utilisé uniquement pour le relais
+} Entrainement;
+
+// Structure pour stocker les informations d'un athlète
+typedef struct {
+    char name[50];               // Nom de l'athlète
+    Entrainement *entrainements; // Détails d'un entraînement spécifique de l'athlète
+    int num_entrainement;        // Nombre d'entrainements enregistrés
+} Athlete;
+
+typedef struct {
+    float meilleur; // Meilleur temps
+    float pire;     // Pire temps
+    float moyenne;  // Moyenne des temps
+} Performances;
+
 
 
