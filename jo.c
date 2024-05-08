79c9ea0c9c99bcb4d2ad2c23644dd476486ac5dc
Ilyesfellah <ilyesfellah.if@gmail.com>
2024-05-08 17:43:51 +0200

    Update jo.c

--- jo.c
+++ jo.c
@@ -35,7 +35,12 @@ typedef struct {
 } Performances;
 
 
-float tempsMoyen() {
+float Tempsmoyen(const Athlete *athlete){
+        float totalTemps=NULL;
+        for(i=0,i<5,i++){
+                totalTemps+=Athlete.temps[i];
+                }
+       return totalTemps/5;
 }
         
 
