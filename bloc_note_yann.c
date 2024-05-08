7022f5fd4116a6265d0eb0fa3fff73f0fb888350
alibababaa <yildizaliulas@gmail.com>
2024-05-08 17:51:51 +0200

    Update bloc_note_yann.c

--- bloc_note_yann.c
+++ bloc_note_yann.c
@@ -1,21 +1,2 @@
-float compareAthlete(Athlete a, Athlete b, Athlete c, Athlete autres) {
-        if (a.moyenne > autres.moyenne && b.moyenne > autres.moyenne && c.moyenne > autres.moyenne) {
-                if(a.moyenne > b.moyenne) {
-                        return a; }
-                else if (b.moyenne > a.moyenne)
-                        return b; }
-                else if (b.moyenne > c.moyenne) {
-                        return b; }
-                else if (c.moyenne > b.moyenne) {
-                        return c; }
-                else if (a.moyenne > c.moyenne)
-                        return a; }
-                else { 
-                        return c; }
-        }
-        else {
-                
-        }
-          
-}
+
   
