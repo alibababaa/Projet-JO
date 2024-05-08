e12177066626c2967896874353da90e9952e33b8
yannagbota <agbotayann@gmail.com>
2024-05-08 18:10:58 +0200

    Update bloc_note_yann.c

--- bloc_note_yann.c
+++ bloc_note_yann.c
@@ -1,4 +1,7 @@
-void tabAthlete( float tabathlete[n+1] , int* n ,tempsmoyen ){
+void tabAthlete( float tabathlete[n+1] , int* n ,tempsmoyen, athlete ){
   
   if (int i=0; i<n ; i++){
-tabathlete[i]=tempsmoyen
+                         tabathlete[i]=tempsmoyen(athlete.(i+1));
+                      }
+           printf("%f";tabathlete[n+1]);
+}
