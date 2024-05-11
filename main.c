0513f615eefef5ca2f035ccd536c29998157f522
yannagbota <agbotayann@gmail.com>
2024-05-11 11:15:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,6 @@
 int main(){
-  
+int f;
+printf("Bonjour\n");
+printf ("Que voulez-vous faire?\n");
+printf("1-entrer et sauvegarder les information d'un nouvel entrainement? 2-consulter l'historique des entrainement? 3-consulter les statistiques de performances des athletes?\n");
+scanf("%d",&f); n  
