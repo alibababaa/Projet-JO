0846568a89ff5328f4cdaca3f1dc9c99ee23cb2d
yannagbota <agbotayann@gmail.com>
2024-05-11 11:23:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,4 +3,17 @@ int f;
 printf("Bonjour\n");
 printf ("Que voulez-vous faire?\n");
 printf("1-entrer et sauvegarder les information d'un nouvel entrainement? 2-consulter l'historique des entrainement? 3-consulter les statistiques de performances des athletes?\n");
-scanf("%d",&f); n  
+scanf("%d",&f);
+if (f==1){
+    ....
+      }
+      elseif(f==2){
+            copieinfo();
+    }
+         elseif(f==3){
+         ....
+           }
+      else{
+           printf("erreur saisie");
+    }
+  
