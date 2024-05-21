44274aa74c364083c46abcc98851ccabda9bd14d
yannagbota <agbotayann@gmail.com>
2024-05-21 13:49:17 +0200

    Update Bibli.h

--- Bibli.h
+++ Bibli.h
@@ -6,17 +6,21 @@
 
 
 // Structure pour stocker les differentes épreuves 
-typedef struct{
-        char epreuveA;    //100m
-        char epreuveB;    //400m
-        char epreuveC;    //5000m
-        char epreuveD;    //marathon
-        char epreuveE;    //4*400m
+typedef enum{
+         epreuveA,    //100m
+         epreuveB,    //400m
+         epreuveC,    //5000m
+         epreuveD,    //marathon
+         epreuveE    //4*400m
 } Epreuve;
-
+typedef struct  {
+    int jour;
+    int mois;
+    int années;
+}Date;
 // Structure pour stocker les informations d'un entraînement
 typedef struct {
-    char date[11];       // Date de l'épreuve
+    Date date;       // Date de l'épreuve
     Epreuve epreuve[20]; // Type d'épreuve
     float temps;         // Temps de la performance
     int position;        // Utilisé uniquement pour le relais
@@ -27,7 +31,7 @@ typedef struct {
     char nom[50];               // Nom de l'athlète
     Entrainement *entrainements; // Détails d'un entraînement spécifique de l'athlète
     int num_entrainement;        // Nombre d'entrainements enregistrés
-    float temps[];               // Temps pour chaque épreuves
+    float* temps;               // Temps pour chaque épreuves
 } Athlete;
 
 typedef struct {
@@ -41,11 +45,7 @@ typedef struct {
      int secondes;
      int millisecondes;
 }heure;
-typedef struct  {
-    int jour;
-    int mois;
-    int années;
-}Date;
+
 
 // prototype des fonctions
 float Tempsmoyen();
@@ -56,4 +56,4 @@ void meilleursAthlete(Athlete a, Athlete b, Athlete c, Athlete autres, Athlete *
     // Tableau pour stocker les 4 moyennes
 float evolutionPerformance(const char *nomFichier, const char *date1, const char *date2) ;
 Entrainement creerNouvelentrainement();
-void sauvegarderentrainement(const char*nouvelentrainement,const Entrainement*entrainement);
+void sauvegarderentrainement(const char*nouvelentrainement,Entrainement entrainement);
