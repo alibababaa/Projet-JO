a4750da9782eede3ca115b470017e398eb571e55
yannagbota <agbotayann@gmail.com>
2024-05-23 12:19:14 +0200

    Create sauvegarderentrainement.c

---
+++ sauvegarderentrainement.c
@@ -0,0 +1,17 @@
+// Fonction pour enregistrer le nouvel entrainement dans un fichier
+void sauvegarderEntrainement(const char* nomFichier, Entrainement entrainement) {
+    FILE* fichier = fopen(nomFichier, "a");
+    if (fichier == NULL) {
+        printf("Erreur : impossible d'ouvrir le fichier\n");
+        return;
+    }
+    fprintf(fichier, "%02d/%02d/%d %d %.2f %d\n",
+            entrainement.date.jour,
+            entrainement.date.mois,
+            entrainement.date.annee,
+            entrainement.epreuve,
+            entrainement.temps,
+            entrainement.position);
+    fclose(fichier);
+    printf("Votre entrainement a bien été sauvegardé\n");
+}
