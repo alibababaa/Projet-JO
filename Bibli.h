ca7779793c5ca0f4afd37a422033495304192abf
yannagbota <agbotayann@gmail.com>
2024-05-23 10:55:44 +0200

    Update Bibli.h

--- Bibli.h
+++ Bibli.h
@@ -4,56 +4,55 @@
 #include <time.h>
 #include <math.h>
 
-
-// Structure pour stocker les differentes épreuves 
-typedef enum{
-         epreuveA,    //100m
-         epreuveB,    //400m
-         epreuveC,    //5000m
-         epreuveD,    //marathon
-         epreuveE    //4*400m
+// Structure pour stocker les différentes épreuves
+typedef enum {
+    epreuveA, //"100m"
+    epreuveB, //"400m",
+    epreuveC, //"5000m",
+    epreuveD, //"marathon",
+    epreuveE  //"4*400m"
 } Epreuve;
-typedef struct  {
+
+// Tableau de noms d'épreuves correspondant à l'énumération
+const char* nomEpreuve[] = {
+    "100m",
+    "400m",
+    "5000m",
+    "marathon",
+    "4*400m"
+};
+
+// Structure pour stocker la date
+typedef struct {
     int jour;
     int mois;
-    int années;
-}Date;
+    int annee;
+} Date;
+
 // Structure pour stocker les informations d'un entraînement
 typedef struct {
     Date date;       // Date de l'épreuve
-    Epreuve epreuve[20]; // Type d'épreuve
-    float temps;         // Temps de la performance
-    int position;        // Utilisé uniquement pour le relais
+    Epreuve epreuve; // Type d'épreuve
+    float temps;     // Temps de la performance
+    int position;    // Utilisé uniquement pour le relais
 } Entrainement;
 
 // Structure pour stocker les informations d'un athlète
 typedef struct {
     char nom[50];               // Nom de l'athlète
-    Entrainement *entrainements; // Détails d'un entraînement spécifique de l'athlète
-    int num_entrainement;        // Nombre d'entrainements enregistrés
-    float* temps;               // Temps pour chaque épreuves
+    Entrainement* entrainements; // Détails des entraînements spécifiques de l'athlète
+    int num_entrainement;        // Nombre d'entraînements enregistrés
+    float* temps;               // Temps pour chaque épreuve
 } Athlete;
 
-typedef struct {
-    float meilleur; // Meilleur temps
-    float pire;     // Pire temps
-    float moyenne;  // Moyenne des temps
-} Performances;
-typedef struct {
-     int heure;
-     int minutes;
-     int secondes;
-     int millisecondes;
-}heure;
-
-
-// prototype des fonctions
+// Prototypes des fonctions
 float Tempsmoyen();
 void tabAthlete();
 void copieinfo();
-void afficherHistorique(Athlete athlete) ;
-void meilleursAthlete(Athlete a, Athlete b, Athlete c, Athlete autres, Athlete *top3) ;
-    // Tableau pour stocker les 4 moyennes
-float evolutionPerformance(const char *nomFichier, const char *date1, const char *date2) ;
-Entrainement creerNouvelentrainement();
-void sauvegarderentrainement(const char*nouvelentrainement,Entrainement entrainement);
+void afficherHistorique(Athlete athlete);
+void meilleursAthlete(Athlete a, Athlete b, Athlete c, Athlete autres, Athlete* top3);
+float evolutionPerformance(const char* nomFichier, const char* date1, const char* date2);
+Entrainement creerNouvelEntrainement();
+void sauvegarderEntrainement(const char* nomFichier, Entrainement entrainement);
+Athlete* lireAthlete(const char* nomFichier);
+void trouverMeilleursAthletes(const char* nomFichier, Athlete* meilleursAthletes[3]);
