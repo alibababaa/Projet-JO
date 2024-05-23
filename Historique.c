88c2b8f17f041259a42f4eb7eb07b391d5c5986c
yannagbota <agbotayann@gmail.com>
2024-05-23 14:20:20 +0200

    Update Historique.c

--- Historique.c
+++ Historique.c
@@ -51,49 +51,38 @@ void copieinfo() {
         afficherHistorique(athletes[i]);
     }
 }
-void chargerDonneesAthletes(Athlete athletes[], char* nom_fichier) {
-    printf("Début du chargement des données des athlètes...\n");
-
-    for (int i = 0; i < 100; i++) {
-        char nom_fichier[100];
-        sprintf(nom_fichier, "%d.txt", i + 1);
-        FILE* fichier = fopen(nom_fichier, "r");
-        if (fichier != NULL) {
-            printf("Fichier %s ouvert avec succès.\n", nom_fichier);
-            // Extrait le nom de l'athlète du nom du fichier
-            char nom_athlete[50];
-            sscanf(nom_fichier, "%[^.]", nom_athlete);
-            strcpy(athletes[*nom_athlete].nom, nom_athlete);
-            athletes[*nb_athletes].num_entrainement = 0;
-            char ligne[100];
-            while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-                printf("Ligne lue : %s\n", ligne);
-                char* token = strtok(ligne, " ");
-                if (token != NULL) {
-                    Date date;
-                    if (sscanf(token, "%d/%d/%d", &date.jour, &date.mois, &date.annee) == 3) {
-                        printf("Date : %d/%d/%d\n", date.jour, date.mois, date.annee);
-                    }
-                    else {
-                        printf("Erreur de format de la date.\n");
-                    }
-
-                    token = strtok(NULL, " ");
-                    athletes[*nb_athletes].entrainements[athletes[*nb_athletes].num_entrainement].epreuve = atoi(token);
-                    printf("Epreuve : %d\n", athletes[*nb_athletes].entrainements[athletes[*nb_athletes].num_entrainement].epreuve);
-                    
-                    token = strtok(NULL, " ");
-                    athletes[*nb_athletes].entrainements[athletes[*nb_athletes].num_entrainement].temps = atof(token);
-                    printf("Temps : %f\n", athletes[*nb_athletes].entrainements[athletes[*nb_athletes].num_entrainement].temps);
-                    
-                    athletes[*nb_athletes].num_entrainement++;
+void chargerDonneesAthlete(Athlete* athlete, const char* nom_fichier) {
+    FILE* fichier = fopen(nom_fichier, "r");
+    if (fichier != NULL) {
+        printf("Chargement des données pour l'athlète %s...\n", athlete->nom);
+        athlete->num_entrainement = 0;
+        char ligne[100];
+        while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+            char* token = strtok(ligne, " ");
+            if (token != NULL) {
+                Date date;
+                if (sscanf(token, "%d/%d/%d", &date.jour, &date.mois, &date.annee) == 3) {
+                    printf("Date : %d/%d/%d\n", date.jour, date.mois, date.annee);
                 }
-            }
-            fclose(fichier);
-            (*nb_athletes)++;
-            printf("Fichier %s fermé.\n", nom_fichier);
-        }
-    }
+                else {
+                    printf("Erreur de format de la date.\n");
+                }
 
-    printf("Fin du chargement des données des athlètes.\n");
+                token = strtok(NULL, " ");
+                athlete->entrainements[athlete->num_entrainement].epreuve = atoi(token);
+                printf("Epreuve : %d\n", athlete->entrainements[athlete->num_entrainement].epreuve);
+                
+                token = strtok(NULL, " ");
+                athlete->entrainements[athlete->num_entrainement].temps = atof(token);
+                printf("Temps : %f\n", athlete->entrainements[athlete->num_entrainement].temps);
+                
+                athlete->num_entrainement++;
+            }
+        }
+        fclose(fichier);
+        printf("Données pour l'athlète %s chargées avec succès.\n", athlete->nom);
+    }
+    else {
+        printf("Impossible d'ouvrir le fichier %s pour l'athlète %s.\n", nom_fichier, athlete->nom);
+    }
 }
