0c42fba15f246c50af55fa2b3f2e7dea55e148c4
yannagbota <agbotayann@gmail.com>
2024-05-23 11:03:07 +0200

    Update nventrainement.c

--- nventrainement.c
+++ nventrainement.c
@@ -1,4 +1,73 @@
 #include"Bibli.h"
+Entrainement creerNouvelEntrainement() {
+    Entrainement nouvelEntrainement;
+    int valid, verif;
+    Date date;
+    do {
+        // Demander à l'utilisateur de saisir la date au format jour/mois/année
+        printf("Entrez une date au format jour/mois/année : ");
+        verif = scanf("%d/%d/%d", &date.jour, &date.mois, &date.annee);
+
+        // Vérifier si la date est valide
+        if ((date.jour >= 1 && date.jour <= 31) &&
+            (date.mois >= 1 && date.mois <= 12) &&
+            (date.annee >= 2020 && date.annee <= 2024) && verif == 3) {
+            valid = 1; // La date est valide, sortir de la boucle
+        }
+        else {
+            valid = 0;
+            printf("Date invalide. Assurez-vous que le jour est entre 1 et 31, le mois est entre 1 et 12, et l'année est entre 2020 et 2024.\n");
+        }
+    } while (!valid);
+
+    nouvelEntrainement.date = date;
+
+    printf("Quel est le type d'épreuve (0:100m, 1:400m, 2:5000m, 3:marathon, 4:4*400m) : ");
+    int epreuve;
+    scanf("%d", &epreuve);
+    nouvelEntrainement.epreuve = (Epreuve)epreuve;
+
+    if (epreuve < 0 || epreuve > 4) {
+        printf("Erreur de saisie\n");
+        exit(1);
+    }
+
+    printf("Saisir le temps de l'athlete (en secondes) : ");
+    scanf("%f", &nouvelEntrainement.temps);
+
+    if (nouvelEntrainement.epreuve == epreuveE) {
+        printf("Quelle était la position de l'athlete : ");
+        verif = scanf("%d", &nouvelEntrainement.position);
+        if (nouvelEntrainement.position < 1 || nouvelEntrainement.position > 4 || verif != 1) {
+            printf("Erreur de saisie, veuillez saisir un chiffre entre 1 et 4\n");
+            exit(1);
+        }
+    }
+    else {
+        nouvelEntrainement.position = -1; // Position non applicable
+    }
+
+    return nouvelEntrainement;
+}
+
+// Fonction pour enregistrer le nouvel entrainement dans un fichier
+void sauvegarderEntrainement(const char* nomFichier, Entrainement entrainement) {
+    FILE* fichier = fopen(nomFichier, "a");
+    if (fichier == NULL) {
+        printf("Erreur : impossible d'ouvrir le fichier\n");
+        return;
+    }
+    fprintf(fichier, "%02d/%02d/%d %d %.2f %d\n",
+            entrainement.date.jour,
+            entrainement.date.mois,
+            entrainement.date.annee,
+            entrainement.epreuve,
+            entrainement.temps,
+            entrainement.position);
+    fclose(fichier);
+    printf("Votre entrainement a bien été sauvegardé\n");
+}
+
 
 /**
 Entrainement* creerNouvelEntrainement() {
